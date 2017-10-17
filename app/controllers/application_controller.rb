9aef31f2de3874ea8df992a76525c077b3e8a4d5
Tomas Espina <tespintom@icloud.com>
2017-10-17 13:45:04 +0000

    fixed def in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by rasinsing an exception
   protect_from_forgery with: :exception
 end
--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,18 +3,22 @@ class QuotesController < ApplicationController
     @quote = Quote.order("RANDOM()").first
   end
 
-  
-
   def create
     @quote = Quote.create(quote_params)
     if @quote.invalid?
-     flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
-  end
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+    end
     redirect_to root_path
-  
   end
 
   def about
+  end
+
+  def show
+    @quote = Quote.where(:id => params[:id]).first
+      if @quote.blank?
+      render :text => "Not Found", :status => :not_found
+    end
   end
 
   private
